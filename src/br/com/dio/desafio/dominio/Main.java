95ec4c88578d8c9600e6047d5e0b92a79fe18f01
Elina Batista <92680117+31ina-Batist4@users.noreply.github.com>
2022-07-06 23:17:31 -0300

    implementando herancas e polimorfismo

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -19,8 +19,41 @@ public class Main {
        mentoria1.setDescricao("Descrição: mentoria java");
        mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
+    /*    System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição: Bootcamp Java Developr");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev dev = new Dev();
+        dev.setNome("Elina");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("*---------------------*");
+        System.out.println(dev.getNome() +": conteudos inscritos "+ dev.getConteudosInscrito());
+        System.out.println("*---------------------*");
+        dev.progredir();
+        dev.progredir();
+        dev.progredir();
+        System.out.println(dev.getNome() +": conteudos concluidos "+ dev.getConteudosConcluidos());
+        System.out.println(dev.getNome() +": conteudos inscritos "+ dev.getConteudosInscrito());
+        System.out.println("XP: "+ dev.calcularTotalXp());
+        Dev dev1 = new Dev();
+        System.out.println("*---------------------*");
+        dev1.setNome("Janaiana");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println(dev1.getNome() + ": conteudos inscritos "+ dev1.getConteudosInscrito());
+        System.out.println("*---------------------*");
+        dev1.progredir();
+        System.out.println(dev1.getNome() + ": conteudos concluidos"+ dev1.getConteudosConcluidos());
+        System.out.println(dev1.getNome() + ": conteudos inscritos "+ dev1.getConteudosInscrito());
+        System.out.println("XP: "+ dev1.calcularTotalXp());
+        System.out.println("*---------------------*");
+
+        System.out.println("Devs inscritos no bootcamp: "+ bootcamp.getNome()+ "\n" +bootcamp.getDevsInscritos());
     }
 }
